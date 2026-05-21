2009066e93b988f398712c2ade9da1d3a1ab943c
Dema Adzhani <demaadzhani08@gmail.com>
2026-05-21 12:26:57 +0700

    push project2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/gallery-orbit.css', 'resources/js/app.js', 'resources/js/frontend/orbit-gallery.js'],
+            input: ['resources/css/app.css', 'resources/css/gallery-orbit.css', 'resources/css/google-translate.css', 'resources/js/app.js', 'resources/js/frontend/orbit-gallery.js', 'resources/js/frontend/google-translate.js'],
             refresh: true,
         }),
         tailwindcss(),
