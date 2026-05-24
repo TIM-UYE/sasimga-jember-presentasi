836d8962105dfe31c258ebbb12a462dc1fcad683
DymasGanteng <dymasramadhan033@gmail.com>
2026-05-24 16:40:52 +0700

    Update create.blade.php

--- resources/views/admin/galeri/create.blade.php
+++ resources/views/admin/galeri/create.blade.php
@@ -11,7 +11,7 @@
         </ul>
     </div>
 @endif
-
+/
 //form untuk tambah galeri baru
 <div class="space-y-6">
     <div class="flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
